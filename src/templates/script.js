676dcf57c9c5e832c5f2440f5be8b1cc20be4f0f
Andrés Felipe Torres <dev@felipeandres254.io>
2019-03-03 01:55:08 -0500

    Add zoom events

--- src/templates/script.js
+++ src/templates/script.js
@@ -20,5 +20,33 @@
             images.querySelectorAll('img#zoom-14')[0].style.display = 'block'
             images.style.display = 'block'
         })
+        document.getElementById('btn-zoom-in').addEventListener('click', function() {
+            var images = document.querySelectorAll('body>div#images')[0]
+            var zoom = parseInt(images.getAttribute('data-zoom'))
+            if (zoom>=12)
+                document.getElementById('btn-zoom-out').removeAttribute('disabled')
+            if (zoom<16) {
+                images.setAttribute('data-zoom', zoom+1)
+                images.querySelectorAll('img#zoom-'+(zoom))[0].style.display = 'none'
+                images.querySelectorAll('img#zoom-'+(zoom+1))[0].style.display = 'block'
+            }
+            if (zoom==15) {
+                this.setAttribute('disabled', 'disabled')
+            }
+        })
+        document.getElementById('btn-zoom-out').addEventListener('click', function() {
+            var images = document.querySelectorAll('body>div#images')[0]
+            var zoom = parseInt(images.getAttribute('data-zoom'))
+            if (zoom<=16)
+                document.getElementById('btn-zoom-in').removeAttribute('disabled')
+            if (zoom>12) {
+                images.setAttribute('data-zoom', zoom-1)
+                images.querySelectorAll('img#zoom-'+(zoom))[0].style.display = 'none'
+                images.querySelectorAll('img#zoom-'+(zoom-1))[0].style.display = 'block'
+            }
+            if (zoom==12) {
+                this.setAttribute('disabled', 'disabled')
+            }
+        })
     })
 })()
