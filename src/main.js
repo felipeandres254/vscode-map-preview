b5f84abd9d7c1b153898af48e382638a7732829b
Andrés Felipe Torres <dev@felipeandres254.io>
2019-03-02 23:31:00 -0500

    Add webview with initial loading status

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // Module imports
 const vscode = require('vscode')
+const path   = require('path')
 const MapPreview = require('./MapPreview')
 
 /**
@@ -11,7 +12,12 @@ exports.activate = (context) => {
     context.subscriptions.push(
         vscode.commands.registerCommand('map.previewLocation', () => {
             MapPreview.getLocation(() => {
-                // TODO Create webview
+                const panel = vscode.window.createWebviewPanel(
+                    'map-preview', 'Preview location', vscode.ViewColumn.One, {
+                        enableScripts: true,
+                    }
+                )
+                panel.webview.html = createWebview(context.extensionPath)
             }).then((location) => {
                 MapPreview.getLocationImages(location).then((images) => {
                     // TODO Update webview
@@ -26,3 +32,37 @@ exports.activate = (context) => {
 
 // This method is called when your extension is deactivated
 exports.deactivate = () => {}
+
+/***** WEBVIEW CONTENT *****/
+const createWebview = (extensionPath) => {
+    const resource = (file) => {
+        return vscode.Uri.file(
+            path.join(extensionPath, `src/${file}`)
+        ).with({scheme: 'vscode-resource'})
+    }
+    const nonce = (() => {
+        let text = '';
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (let i = 0; i < 32; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    })()
+
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}'; style-src vscode-resource:;">
+    <title>Preview location</title>
+    <link rel="stylesheet" type="text/css" href="${resource('templates/style.css')}">
+</head>
+<body>
+    <div id="loading">
+        <img src="${resource('images/loading.svg')}">
+        <p>Loading preview...</p>
+    </div>
+</body>
+</html>`
+}
