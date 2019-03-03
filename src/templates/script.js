9e94f47d3e2193fd4383ca0f35193da886ce6321
Andrés Felipe Torres <dev@felipeandres254.io>
2019-03-03 02:01:13 -0500

    Set zoom constants

--- src/templates/script.js
+++ src/templates/script.js
@@ -2,6 +2,10 @@
 /* eslint-disable no-undef */
 
 (function() {
+    var ZOOM = {
+        DEFAULT: 14,
+        MIN: 12, MAX: 16,
+    }
     document.addEventListener('DOMContentLoaded', function() {
         window.addEventListener('message', function(event) {
             if (event.data.type!=='load')
@@ -12,39 +16,39 @@
                 let img = document.createElement('img')
                 img.src = image
                 img.style.display = 'none'
-                img.setAttribute('id', 'zoom-'+(index+12))
+                img.setAttribute('id', 'zoom-'+(index+ZOOM.MIN))
                 images.insertBefore(img, images.firstChild)
             })
             loading.style.display = 'none'
-            images.setAttribute('data-zoom', 14)
-            images.querySelectorAll('img#zoom-14')[0].style.display = 'block'
+            images.setAttribute('data-zoom', ZOOM.DEFAULT)
+            images.querySelectorAll('img#zoom-'+(ZOOM.DEFAULT))[0].style.display = 'block'
             images.style.display = 'block'
         })
         document.getElementById('btn-zoom-in').addEventListener('click', function() {
             var images = document.querySelectorAll('body>div#images')[0]
             var zoom = parseInt(images.getAttribute('data-zoom'))
-            if (zoom>=12)
+            if (zoom>=ZOOM.MIN)
                 document.getElementById('btn-zoom-out').removeAttribute('disabled')
-            if (zoom<16) {
+            if (zoom<ZOOM.MAX) {
                 images.setAttribute('data-zoom', zoom+1)
                 images.querySelectorAll('img#zoom-'+(zoom))[0].style.display = 'none'
                 images.querySelectorAll('img#zoom-'+(zoom+1))[0].style.display = 'block'
             }
-            if (zoom==15) {
+            if (zoom==(ZOOM.MAX-1)) {
                 this.setAttribute('disabled', 'disabled')
             }
         })
         document.getElementById('btn-zoom-out').addEventListener('click', function() {
             var images = document.querySelectorAll('body>div#images')[0]
             var zoom = parseInt(images.getAttribute('data-zoom'))
-            if (zoom<=16)
+            if (zoom<=ZOOM.MAX)
                 document.getElementById('btn-zoom-in').removeAttribute('disabled')
-            if (zoom>12) {
+            if (zoom>ZOOM.MIN) {
                 images.setAttribute('data-zoom', zoom-1)
                 images.querySelectorAll('img#zoom-'+(zoom))[0].style.display = 'none'
                 images.querySelectorAll('img#zoom-'+(zoom-1))[0].style.display = 'block'
             }
-            if (zoom==12) {
+            if (zoom==(ZOOM.MIN+1)) {
                 this.setAttribute('disabled', 'disabled')
             }
         })
